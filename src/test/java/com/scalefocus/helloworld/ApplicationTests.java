0e8858894687a853d8b669df46159b7991b2cf05
Metodi Vladimirov <metodi@ntb270.sf.unix>
2021-01-08 11:04:22 +0200

    Add HelloController test

--- src/test/java/com/scalefocus/helloworld/ApplicationTests.java
+++ src/test/java/com/scalefocus/helloworld/ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.scalefocus.helloworld;
 
+import com.scalefocus.helloworld.controller.HelloController;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private HelloController controller;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(this.controller);
 	}
 
 }
