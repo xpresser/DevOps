381da79523b3e4d8f1ff6d567087e0e38b523b20
Metodi Vladimirov <30759581+xpresser@users.noreply.github.com>
2021-01-14 22:35:22 +0200

    Update HelloService.java

--- src/main/java/com/scalefocus/helloworld/service/HelloService.java
+++ src/main/java/com/scalefocus/helloworld/service/HelloService.java
@@ -7,6 +7,6 @@ public class HelloService implements HelloWorld {
 
     @Override
     public String sayHello() {
-        return "Hello, Plovdiv!!\n";
+        return "Hello, Plovdiv!\n";
     }
 }
