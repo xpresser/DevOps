a52d107492ba0920282540242a0e4c60e5511ae9
root <root@prod-env>
2021-01-16 18:09:51 +0000

    Update HelloService

--- src/main/java/com/scalefocus/helloworld/service/HelloService.java
+++ src/main/java/com/scalefocus/helloworld/service/HelloService.java
@@ -7,6 +7,6 @@ public class HelloService implements HelloWorld {
 
     @Override
     public String sayHello() {
-        return "Hello, Nesebar!\n";
+        return "Hello, Scalefocus!\n";
     }
 }
